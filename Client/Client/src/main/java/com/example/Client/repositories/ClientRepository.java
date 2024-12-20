69a834d863b43ac8b4981ee0f66128f96e6292a4
oussama <oussamamarir27@gmail.com>
2024-12-20 13:15:13 +0100

    Added new folders and updated project files

--- Client/Client/src/main/java/com/example/Client/repositories/ClientRepository.java
+++ Client/Client/src/main/java/com/example/Client/repositories/ClientRepository.java
@@ -1,9 +1,2 @@
-package com.example.Client.repositories;
-
-import com.example.Client.entities.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends JpaRepository<Client, Long> {
+package com.example.Client.repositories;public class ClientRepository {
 }
--- Voiture/Voiture/src/main/java/com/example/Voiture/services/ClientService.java
+++ Voiture/Voiture/src/main/java/com/example/Voiture/services/ClientService.java
@@ -1,12 +1,2 @@
-package com.example.Voiture.services;
-
-import com.example.Voiture.entities.Client;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-@FeignClient(name = "SERVICE-CLIENT") // Ensure "SERVICE-CLIENT" matches your Eureka registration
-public interface ClientService {
-    @GetMapping("/clients/{id}")
-    Client getClientById(@PathVariable("id") Long id);
+package com.example.Voiture.services;public class ClientService {
 }
